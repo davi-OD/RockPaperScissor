66eba43963f210615ced7c3a4212aeb0c6349f77
davi-OD <davidodie51@gmail.com>
2021-08-21 21:38:20 +0300

    second page done

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const pickUserHand = (hand) => {
+    console.log(hand);
+
+    //Hide current page
+    let hands = document.querySelector(".hands");
+    hands.style.display = "none"
+    //Show the choice on a page
+    let choice = document.querySelector(".contest");
+
+}
