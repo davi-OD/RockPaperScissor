909a697af6af5109897f120cb18103bd4cd859f8
davi-OD <davidodie51@gmail.com>
2021-08-22 23:19:57 +0300

    started js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,24 @@
+
+const handOptions = {
+    "rock": "../images/icon-rock.svg",
+    "paper": "../images/icon-paper.svg",
+    "scissors": "../images/icon-scissors.svg"
+}
+
 const pickUserHand = (hand) => {
     console.log(hand);
 
     //Hide current page
     let hands = document.querySelector(".hands");
     hands.style.display = "none"
+    
     //Show the choice on a page
-    let choice = document.querySelector(".contest");
+    let contest = document.querySelector(".contest");
+    contest.style.display = "flex";
+
+    //Set the user choice
+    document.getElementById("userPick").src = handOptions[hand];
 
 }
+
+const playAgain = document.querySelector(".newGame");
